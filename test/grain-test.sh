550c592f480d7aa81e12864a6fec659e1ab10ed2
Michele Carignani <michele.carignani@gmail.com>
2015-01-09 15:57:33 +0100

    new tests

--- test/grain-test.sh
+++ test/grain-test.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/bash/
 #
 # Author: Michele Carignani <michele.carignani@gmail.com>
-#
 
 BUILD_DIR=./build
 
-PAR_DEGS="2 4 8 16 32 64"
-
-TIME_FORMAT_STRING="%e\t%U\t%S"
+PAR_DEGS="1 2 4 8 16 32 64 128"
+WORKERS_NUM=16
 
 par_test() {
 	$BUILD_DIR/$1 $2 $3 $4 $5 1>/dev/null
@@ -26,25 +24,23 @@ if [ $# -lt 2 ] ; then
     exit 0
 fi
 
-echo -e "\n# Test SEQ"
-test_sequential $1 $2
-echo -e "\t"
+echo "\n-- DIFFERENT GRAIN TESTS -- "
 
 echo -e "\n# Test FARM"
 for i in $PAR_DEGS ; do
-	par_test "farm" $1 $2 $i
+	par_test "farm" $1 $2 16 $i
 	echo -e "\t"
 done
 
 echo -e "\n# Test PIPE-FARM"
 for i in $PAR_DEGS ; do
-	par_test "pipe-farm" $1 $2 $i
+	par_test "pipe-farm" $1 $2 16 $i
 	echo -e "\t"
 done
 
 echo -e "\n# Test MAP"
 for i in $PAR_DEGS ; do
-	par_test "map" $1 $2 $i
+	par_test "map" $1 $2 16 $i
 	echo -e "\t"
 done
 
