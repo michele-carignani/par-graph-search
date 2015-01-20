49c8712224b37ecd52fdc0587eb4cc96a49d1dd4
Michele Carignani <michele.carignani@gmail.com>
2015-01-20 11:21:38 +0100

    test a pure map scalability

---
+++ src/pure-map.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   pure-map.cpp
+ * Author: Michele Carignani
+ *
+ * Created on 20 gennaio 2015, 10.47
+ */
+
+#include <cstdlib>
+#include <random>
+#include <cmath>
+#include <iostream>
+#include "ff/parallel_for.hpp"
+#include "utils.hpp"
+
+using namespace std;
+using namespace ff;
+
+#define DATA_SIZE 100000
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    struct timespec start, end;
+    
+    double data[DATA_SIZE];
+    
+    for(int j = 0; j < DATA_SIZE; j++){
+        data[j] = rand() % 1000;
+    }
+    
+    cout << "# nworkers, process cpu time\n";
+    for(int j = 1; j < 129; j++){
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+        ParallelFor pf;
+        pf.parallel_for(0, DATA_SIZE, j, [&data](const int i){
+            data[i] = pow(data[i], 7);
+        });    
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        cout << j << ", " << elapsed_time_secs(start, end) << "\n";
+    }
+    
+    return 0;
+}
+
