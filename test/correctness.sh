386f6f08e964186f03c1a55866dfcb8bb65af355
Michele Carignani <michele.carignani@gmail.com>
2015-02-16 17:49:45 +0100

    added correctness test

---
+++ test/correctness.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# 
+# File:   correctness.sh
+# Author: michele.carignani@gmail.com
+#
+#
+
+if [ $# -lt 3 ] ; then
+	echo "Usage: $0 <graph_file> <needles_file> <results_file>"
+	exit
+fi
+
+GRAPH_FILE=$1
+NEEDLES_FILE=$2
+RESULTS_FILE=$3
+
+BIN_DIR=./build
+TMP_DIR=/tmp
+BINS="seq seq-no-io farm farm-no-io map"
+
+sorted_res=$TMP_DIR/$(basename $RESULTS_FILE).sorted
+sort $RESULTS_FILE > $sorted_res
+
+for b in $BINS ; do
+	tmpfile=$TMP_DIR/res_$b_$(date "+%s")
+	./$BIN_DIR/$b $GRAPH_FILE $NEEDLES_FILE > $tmpfile 2>/dev/null
+	sort $tmpfile > $tmpfile.sorted
+	diff $sorted_res $tmpfile.sorted > /dev/null 
+	if [ $? -ne 0 ] ; then
+		echo "$b : fail"
+	else
+		echo "$b : success"
+	fi
+	rm -f $tmpfile $tmpfile.sorted
+done
+
+rm -f $sorted_res
